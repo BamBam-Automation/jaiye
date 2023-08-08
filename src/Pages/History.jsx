842496cb3b8ed69964cd0f5942ad635347a72857
olaDmenace <olayinkafagbenro@gmail.com>
2023-08-08 15:08:15 +0100

    History Card Fixed

--- src/Pages/History.jsx
+++ src/Pages/History.jsx
@@ -6,7 +6,8 @@ const History = () => {
   return (
     <div className="p-7 grid gap-5 items-start">
       <NavBar title={"History"} />
-      <div className="mt-10">
+      <div className="mt-10 grid gap-5">
+        <HistoryCard />
         <HistoryCard />
       </div>
     </div>
