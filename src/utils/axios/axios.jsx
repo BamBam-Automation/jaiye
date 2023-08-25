486b96097441e8fe8a22c9b17e0b544122c62cc4
olaDmenace <olayinkafagbenro@gmail.com>
2023-08-25 15:33:23 +0100

    React Query Installation

--- src/utils/axios/axios.jsx
+++ src/utils/axios/axios.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const axios = axios.create({
   baseURL: "https://jaiye.axle-cartage.com/api/",
+  timeout: 2000,
   Headers: {
     accept: "*/*",
     "Content-Type": "application/json",
