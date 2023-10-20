737b07ff00406b5bee92df6ef462e3cd6967088b
olaDmenace <olayinkafagbenro@gmail.com>
2023-10-20 11:16:47 +0100

    Landing Page Updated

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 // App.js
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useSelector } from 'react-redux'; // Import useSelector to access Redux state
-import Membership from './Pages/Membership';
-import Welcome from './Pages/Welcome';
-import Location from './Pages/Location';
-import Explore from './Pages/Explore';
-import Clubpage from './Pages/Clubpage';
-import Ticket from './Pages/Ticket';
-import QRCode from './Pages/QRCode';
-import History from './Pages/History';
-import Home from './Pages/Home';
-import Profile from './Pages/Profile';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useSelector } from "react-redux"; // Import useSelector to access Redux state
+import Membership from "./Pages/Membership";
+import Welcome from "./Pages/Welcome";
+import Location from "./Pages/Location";
+import Explore from "./Pages/Explore";
+import Clubpage from "./Pages/Clubpage";
+import Ticket from "./Pages/Ticket";
+import QRCode from "./Pages/QRCode";
+import History from "./Pages/History";
+import Home from "./Pages/Home";
+import Profile from "./Pages/Profile";
 
 function App() {
   // Access the isAuthenticated state from Redux store
@@ -21,8 +21,8 @@ function App() {
     <div className="bg-[#F9F9F9] text-tertiary min-h-screen overflow-y-scroll">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Membership />} />
-          <Route path="welcome" element={<Welcome />} />
+          <Route path="/" element={<Welcome />} />
+          <Route path="join" element={<Membership />} />
 
           {/* Protected routes */}
           <Route
--- src/Pages/Welcome.jsx
+++ src/Pages/Welcome.jsx
@@ -10,7 +10,10 @@ const Welcome = () => {
       <img className="absolute top-12" src={Img} alt="" />
       <img className="absolute bottom-12 right-0" src={ImgTwo} alt="" />
       <div className="self-center mt-36 grid justify-items-center">
-        <img src={Jaiye} alt="" />
+        <div className="relative">
+          <img className="animate-ping absolute" src={Jaiye} alt="" />
+          <img src={Jaiye} alt="" />
+        </div>
         <h3 className="text-7xl font-bold">Jaiye</h3>
         <h6 className="font-medium text-[#afafaf]">Explore the Nights</h6>
       </div>
