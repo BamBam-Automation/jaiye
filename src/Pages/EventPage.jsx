c553901e09388285e451c683292e228ae7f45023
olaDmenace <olayinkafagbenro@gmail.com>
2023-10-31 15:28:23 +0100

    Event Restyled

--- src/Pages/EventPage.jsx
+++ src/Pages/EventPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 import NavBar from "../components/NavBar";
-import { Alert, Button } from "@material-tailwind/react";
+import { Alert, Button, Carousel } from "@material-tailwind/react";
 import { CiWarning } from "react-icons/ci";
 import axiosInstance from "../utils/axios/axios";
 import { BsPatchCheck } from "react-icons/bs";
@@ -176,23 +176,28 @@ const EventPage = () => {
   }, 3000);
 
   return (
-    <div className="p-7 grid gap-5 items-start">
+    <div className="p-7 grid gap-5 items-start relative">
       <NavBar title={summary?.name} />
-      <p>{summary?.description}</p>
-      <div className="grid gap-3 relative">
-        {alert && (
-          <Alert
-            animate={{
-              mount: { y: 0 },
-              unmount: { y: 0 },
-            }}
-            color={bgColor}
-            icon={icon}
-            className="absolute w-full h-auto top-8"
-          >
-            {response}
-          </Alert>
-        )}
+      <p className="font-bold text-2xl">{summary?.description}</p>
+      {alert && (
+        <Alert
+          animate={{
+            mount: { y: 0 },
+            unmount: { y: 0 },
+          }}
+          color={bgColor}
+          icon={icon}
+          className="absolute h-auto top-8 w-11/12 right-5"
+        >
+          {response}
+        </Alert>
+      )}
+      <Carousel loop={true} transition={{ duration: 2 }} className="rounded-xl">
+        {summary?.imageUrls?.map((event) => (
+          <img key={event} src={event} alt="event-banner" />
+        ))}
+      </Carousel>
+      <div className="grid gap-3">
         <p className="font-semibold text-lg text-primary">Event Ticket Types</p>
         <p>Select Ticket Type below:</p>
         <div className="grid gap-5">
@@ -202,7 +207,7 @@ const EventPage = () => {
                 selectedTicketType === ticketType.ticketTypeId
                   ? "text-primary"
                   : "bg-white text-primary"
-              } px-4 py-2 text-center border border-primary grid place-content-center`}
+              } px-4 py-2 text-center border border-primary rounded-lg shadow-xl grid place-content-center`}
               key={ticketType.ticketTypeId}
               onClick={() =>
                 handleTicketTypeClick(
@@ -216,10 +221,10 @@ const EventPage = () => {
                 <div className="grid gap-5">
                   {eventDates.map((eventDate) => (
                     <label
-                      className={`border h-10 text-center border-primary grid place-content-center ${
+                      className={`border h-10 text-center rounded-lg shadow-xl border-primary grid place-content-center ${
                         selectedDates.includes(eventDate.eventDateId)
                           ? "bg-primary text-white"
-                          : "bg-white text-primary"
+                          : "bg-primary/10 text-primary"
                       }`}
                       key={eventDate.eventDateId}
                       onClick={() =>
