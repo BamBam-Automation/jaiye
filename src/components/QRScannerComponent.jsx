b07e7d867f5d064f1b808a4a3b9bdbb475d9f0e7
olaDmenace <olayinkafagbenro@gmail.com>
2023-12-07 16:31:37 +0100

    QR Code data fix

--- src/components/QRScannerComponent.jsx
+++ src/components/QRScannerComponent.jsx
@@ -36,7 +36,7 @@ const QRScannerComponent = () => {
           style={scannerStyle}
           constraints={{ video: { facingMode: "environment" } }}
         />
-        {qrCode && <p>Scanned QR Code: {qrCode?.text?.ticketId}</p>}
+        {qrCode && <p>Scanned QR Code: {qrCode?.text}</p>}
       </div>
     </div>
   );
